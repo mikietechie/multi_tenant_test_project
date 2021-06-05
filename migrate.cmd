9ac72a3b7580db3aa0c97b433c601355700e0b3e
mikietechie <mzinyoni7@outlook.com>
2021-06-06 01:27:37 +0200

    all done

--- migrate.cmd
+++ migrate.cmd
@@ -6,3 +6,24 @@ manage.py migrate_schemas
 manage.py makemigrations
 manage.py migrate
 echo did all migrations
+
+echo Do you want to create an initial tenant [y/n]
+set /p wants_to_create_a_tenant = 
+if "%wants_to_create_a_tenant%" == "n" goto has_tenants_already
+echo Copy and Paste the following lines one at a time in your shell
+echo from tenants.models import Tenant
+echo Tenant.objects.create(name='admin', domain='admin.co.zw', schema='public')
+echo exit()
+manage.py shell
+echo Tenant must be successfully created
+goto has_tenants_already
+
+:has_tenants_already
+echo Do you want to create an initial super user for the admin-public schema [y/n]
+set /p wants_to_create_a_superuser = 
+if "%wants_to_create_a_superuser%" == "n" goto finish
+manage.py createsuperuser
+goto finish
+
+:finish
+echo Done!!!
