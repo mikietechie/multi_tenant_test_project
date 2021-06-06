3ab919038d9781c8de03c1fb0c0ef953c64fcd64
mikietechie <mzinyoni7@outlook.com>
2021-06-06 15:13:50 +0200

    ....

---
+++ tenant_context_manage.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'multi_tenant_test_project.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    from django.db import connection
+    args = sys.argv
+    schema = args[1]
+    with connection.cursor() as cursor:
+        cursor.execute(f"SET search_path to {schema}")
+        del args[1]
+        execute_from_command_line(args)
+
+
+if __name__ == '__main__':
+    main()
