55389215e182febee7a8ea75c26dbbb2b280677f
mikietechie <mzinyoni7@outlook.com>
2021-06-06 14:48:25 +0200

    maone

--- tenants/management/commands/migrate_schemas.py
+++ tenants/management/commands/migrate_schemas.py
@@ -8,6 +8,7 @@ def update_schema():
     with connection.cursor() as cursor:
         for tenant in Tenant.objects.all():
             cursor.execute(f"CREATE SCHEMA IF NOT EXISTS {tenant.schema}")
+            cursor.execute(f"SET search_path to {tenant.schema}")
 
 
 class Command(MigrationCommand):
--- tenants/models.py
+++ tenants/models.py
@@ -17,12 +17,43 @@ class Tenant(models.Model):
         return self.name
     
     def save(self, *args, **kwargs):
+        super().save(*args,**kwargs)
+        # TODO: implement correct logic to trigger the creation of a schema on the initial save of a tenant
+        '''
+        currently do the following:
+            - manage.py makemigrations
+            - manage.py migrate
+            - manage.py migrate_schemas
+            - tenant_context_manage.py public createsuperuser
+            - in manage.py shell create a tenant with (name: admin, domain: localhost, schem: public)
+            - visit admin create more tenants
+            - manage.py migrate_schemas
+            - repeat for all tenants
+            - tenant_context_manage.py tenant_schema createsuperuser
+            done
+        '''
+        '''
         if self.pk:
             super().save(*args,**kwargs)
             return
         super().save(*args,**kwargs)
         with connection.cursor() as cursor:
             cursor.execute(f"CREATE SCHEMA IF NOT EXISTS {self.schema}")
+            cursor.execute(f"SET search_path to {self.schema}")
+            python_launcher = 'python' if sys.platform.startswith("win32") else 'python3'
+            os.system(f"{python_launcher} tenant_context_manage.py aone migrate")
+            set_active_db_schema(self.schema)
+            if User.objects.count() == 0:
+                User.objects.create(
+                    username=f'{self.schema} admin',
+                    email=f"{self.schema}@mail.com",
+                    password=f"{self.schema}password",
+                    is_superuser=True
+                )
+            set_active_db_schema("public")
+        '''
+            
+            
         '''
         To add logic to create a super user instance of a new tenant
         set_active_db_schema(self.schema)
